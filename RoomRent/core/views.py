d94d499dc3db008592e081fc3e3028db165a35c4
rohan0005 <khadkarohan143@gmail.com>
2024-03-29 21:53:36 +0545

    Added UI in home page
    1. Added UI in home page.
    2. Changes in header page for navigation bar.
    3. Added checkValidity views to handle auth error.

--- RoomRent/core/views.py
+++ RoomRent/core/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+import sweetify
 
 # Create your views here.
 def index(request):
+    return render(request, 'Landing page/index.html')
+
+def checkValidity(request):
+    sweetify.error(request, 'You are not authorized to view this page.', timer= 8000)
     return render(request, 'Landing page/index.html')
