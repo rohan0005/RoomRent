8d68a41bdb3c88e4a01b1a6c644e6a68920e3196
rohan0005 <khadkarohan143@gmail.com>
2024-04-21 02:15:09 +0545

    Minor changes in UI

--- RoomRent/users/forms.py
+++ RoomRent/users/forms.py
@@ -3,7 +3,6 @@ from django.contrib.auth.forms import User
 from django import forms
 
 class CreateUserForm(UserCreationForm):
-    profile_picture = forms.ImageField(required=False)
     class Meta:
         model= User
         fields = ['first_name', 'last_name','username', 'email', 'password1', 'password2',]
